b243311fe37db13b6d211ffee933f0af4a76f70b
Jon Flash <funkswing@gmail.com>
2013-09-11 13:40:46 -0400

    This is the beginning

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 26 14:35:42 2012
+
+@author: jharston
+"""
+
+from disutils.core import setup
+
+setup(
+        name            = 'ubertool',
+        version         = '1.0.0',
+        py_modules      = ['ubertool'],
+        author          = 'ubertool development',
+        author_email    = 'ecoubertool@gmail.com',
+        url             = 'http://www.ubertool.org',
+        description     = '&uuml;bertool for EFED ecological risk modules',
+        )
